fd8fbca2964f50dfc735f9485e1fd48e1dcb8759
Justin_Jung <jungdw0624@gmail.com>
2019-06-20 12:05:07 +0900

    Initial Commit

---
+++ JustinWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JustinWeatherApp
+//
+//  Created by Dong Wook Jung on 20/06/2019.
+//  Copyright © 2019 Dong Wook Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
