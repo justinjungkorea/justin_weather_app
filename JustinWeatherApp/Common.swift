b958090e77d8a5e8e7120b972e6bb62c6e886532
Dong Wook Jung <jungdw0624@gmail.com>
2019-07-11 16:41:00 +0900

    Application Key for T developer

---
+++ JustinWeatherApp/Common.swift
@@ -0,0 +1,11 @@
+//
+//  Common.swift
+//  JustinWeatherApp
+//
+//  Created by Dong Wook Jung on 11/07/2019.
+//  Copyright © 2019 Dong Wook Jung. All rights reserved.
+//
+
+import Foundation
+
+let appkey = "937160a0-5132-470d-abdc-f22b0bd62c94"
