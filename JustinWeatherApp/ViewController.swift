a44a5d17946a0be393fe76d9f6652509d71c1635
Dong Wook Jung <jungdw0624@gmail.com>
2019-07-16 22:04:23 +0900

    weather data print

--- JustinWeatherApp/ViewController.swift
+++ JustinWeatherApp/ViewController.swift
@@ -10,12 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let tempFormatter: NumberFormatter = {
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 1
+        
+        return formatter
+    }()
+    
+    let dateFormatter: DateFormatter = {
+       let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "Ko_kr")
+        return formatter
+    }()
+    
     @IBOutlet weak var listTableView: UITableView!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        WeatherDataSource.shared.fetchSummary(lat: 37.498206, lon: 127.02761){
+            [weak self] in self?.listTableView.reloadData()
+        }
+        
+        WeatherDataSource.shared.fetchForecast(lat: 37.498206, lon: 127.02761){
+            [weak self] in self?.listTableView.reloadData()
+        }
     }
 
 
@@ -31,7 +53,7 @@ extension ViewController: UITableViewDataSource{
         case 0:
             return 1
         case 1:
-            return 0
+            return WeatherDataSource.shared.forecastList.count
         default:
             return 0
         }
@@ -39,12 +61,44 @@ extension ViewController: UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.section == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: SummaryTableViewCell.identifier, for: indexPath)
+            let cell = tableView.dequeueReusableCell(withIdentifier: SummaryTableViewCell.identifier, for: indexPath) as! SummaryTableViewCell
+            
+            if let data = WeatherDataSource.shared.summary?.weather.minutely.first{
+                cell.weatherImageView.image = UIImage(named: data.sky.code)
+                cell.statusLabel.text = data.sky.name
+                
+                let max = Double(data.temperature.tmax) ?? 0.0
+                let min = Double(data.temperature.tmin) ?? 0.0
+                
+                let maxStr = tempFormatter.string(for: max) ?? "-"
+                let minStr = tempFormatter.string(for: min) ?? "-"
+                
+                cell.minMaxLabel.text = "최대 \(maxStr)℃ 최소 \(minStr)℃"
+                
+                let current = Double(data.temperature.tc) ?? 0.0
+                let currentStr = tempFormatter.string(for: current) ?? "-"
+                
+                cell.currentTemperatureLabel.text = "\(currentStr)℃ "
+            }
             
             return cell
         }
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: ForecastTableViewCell.identifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: ForecastTableViewCell.identifier, for: indexPath) as! ForecastTableViewCell
+        
+        let target = WeatherDataSource.shared.forecastList[indexPath.row]
+        
+        dateFormatter.dateFormat = "M.d (E)"
+        cell.dateLabel.text = dateFormatter.string(for: target.date)
+        
+        dateFormatter.dateFormat = "HH:00"
+        cell.timeLabel.text = dateFormatter.string(for: target.date)
+        
+        cell.weatherImageView.image = UIImage(named: target.skyCode)
+        cell.statusLabel.text = target.skyName
+        
+        let tempStr = tempFormatter.string(for: target.temperature) ?? "-"
+        cell.temperatureLabel.text = "\(tempStr)℃"
         
         return cell
     }
